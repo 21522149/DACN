b6ce63bb1e1bb673c147cc4067fed2f99c15b358
21522149 <thanhhuy2017tv@gmail.com>
2025-07-11 03:30:26 +0700

    modify make-docker.ps1

--- Scripts/make-docker.ps1
+++ Scripts/make-docker.ps1
@@ -1,19 +1,22 @@
+# make-docker.ps1
 $ErrorActionPreference = "Stop"
 
 $SCRIPTDIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Write-Host "Script Directory: $SCRIPTDIR"
+Write-Host "Script directory: $SCRIPTDIR"
 
 $TAG = $env:BUILD_NUMBER
 $AWS_DEFAULT_REGION = $env:AWS_DEFAULT_REGION
 $REPO_PREFIX = $env:REPO_PREFIX
 
-Write-Host "Build Tag: $TAG"
+Write-Host "Tag: $TAG"
 Write-Host "Repo Prefix: $REPO_PREFIX"
 
-$dirs = Get-ChildItem -Path "$SCRIPTDIR\..\src" -Directory
+$srcPath = Join-Path $SCRIPTDIR "..\src"
+$directories = Get-ChildItem -Path $srcPath -Directory
 
-foreach ($dir in $dirs) {
+foreach ($dir in $directories) {
     $svcname = $dir.Name
+
     if ($svcname.StartsWith(".")) {
         Write-Host "Skipping hidden directory: $svcname"
         continue
@@ -21,7 +24,7 @@ foreach ($dir in $dirs) {
 
     $builddir = $dir.FullName
     if ($svcname -eq "cartservice") {
-        $builddir = Join-Path $dir.FullName "src"
+        $builddir = Join-Path $builddir "src"
     }
 
     Set-Location $builddir
@@ -29,13 +32,14 @@ foreach ($dir in $dirs) {
     docker system prune -f
     docker container prune -f
 
-    $image = "$REPO_PREFIX$svcname:$TAG"
+    $image = "${REPO_PREFIX}${svcname}:${TAG}"
     Write-Host "Building and pushing: $image"
 
     aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $REPO_PREFIX
-    docker build -t $svcname .
-    docker tag $svcname $image
-    docker push $image
+
+    docker build -t "$svcname" .
+    docker tag "$svcname" "$image"
+    docker push "$image"
 }
 
-Write-Host "✅ Successfully built and pushed all the images"
+Write-Host "Successfully built and pushed all the images."
